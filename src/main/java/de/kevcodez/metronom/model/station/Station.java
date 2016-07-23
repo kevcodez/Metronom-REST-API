f26d10726ae99e2f71636f39114d2e289a8a5a85
kevcodez <k.grueneberg1994@gmail.com>
2016-07-23 17:54:20 +0200

    SonarQube Feedback

--- src/main/java/de/kevcodez/metronom/model/station/Station.java
+++ src/main/java/de/kevcodez/metronom/model/station/Station.java
@@ -22,6 +22,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -75,7 +76,7 @@ public class Station {
 
   @JsonIgnore
   public List<String> getAlternativeNames() {
-    return alternativeNames;
+    return Collections.unmodifiableList(alternativeNames);
   }
 
   @Override
